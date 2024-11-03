be75d11e45787807c6adda721ccb54733db8fb1d
Wilson Birch <wilsonbirch@gmail.com>
2024-11-02 22:53:41 -0400

    first commit

---
+++ app/server.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import 'dotenv/config'
+
+import rootHtml from './html/root'
+import { nodeResque } from './utils/node-resque'
+
+const app = express()
+
+app.use(express.static('app/public'))
+
+// Example defining a route in Express
+app.get('/', (req: Request, res: any) => {
+	res.send(rootHtml)
+})
+
+// Use routes
+// app.use('/home', homeRoute)
+
+nodeResque()
+
+// Example specifying the port and starting the server
+const port = process.env.PORT || 3000 // You can use environment variables for port configuration
+
+app.listen(port, () => {
+	console.log(`Server is running: http://localhost:${port}/ `)
+})
