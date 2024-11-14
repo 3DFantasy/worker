2c8e1c2aa83aea1d202cebade6ff33a1dcc13cae
Wilson Birch <wilsonbirch@gmail.com>
2024-11-13 23:14:56 -0500

    updated prisma schema

--- prisma/migrations/20241114041345_account_team_depthchart_tables/migration.sql
+++ prisma/migrations/20241114041345_account_team_depthchart_tables/migration.sql
@@ -12,12 +12,26 @@ CREATE TABLE "Account" (
 -- CreateTable
 CREATE TABLE "Team" (
     "id" SERIAL NOT NULL,
-    "depthChartArray" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "DepthChart" (
+    "id" TEXT NOT NULL,
+    "teamId" INTEGER NOT NULL,
+    "value" TEXT,
+    "year" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DepthChart_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Account_email_key" ON "Account"("email");
+
+-- AddForeignKey
+ALTER TABLE "DepthChart" ADD CONSTRAINT "DepthChart_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
